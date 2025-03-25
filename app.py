54c23f9d84b2f7d561752e0f4e602febb84b783d
elianpullen <elianpullen@protonmail.com>
2025-03-25 21:23:49 +0100

    feat: exercise crud

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for    
 from models import db, Exercise
+from routes import init_routes
 import os
 
 app = Flask(__name__)
@@ -16,41 +17,7 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)  # Initialize db with app
 
-@app.route("/")
-def index():
-    return render_template("index.html", name="Henk")
-
-# Home route - List all exercises
-@app.route('/exercise')
-def exercise():
-    exercises = Exercise.query.all()
-    return render_template('admin/exercise/index.html', exercises=exercises)
-
-@app.route('/edit/<int:id>', methods=['GET', 'POST'])
-def edit_exercise(id):
-    exercise = Exercise.query.get_or_404(id)
-    if request.method == 'POST':
-        exercise.name = request.form['name']
-        db.session.commit()
-        return redirect(url_for('index'))
-    return render_template('admin/exercise/edit.html', exercise=exercise)
-
-@app.route('/exercise/add', methods=['GET', 'POST'])
-def add_exercise():
-    if request.method == 'POST':
-        name = request.form['name']
-        new_exercise = Exercise(name=name)
-        db.session.add(new_exercise)
-        db.session.commit()
-        return redirect(url_for('index'))
-    return render_template('admin/exercise/add.html', exercise=exercise)
-
-@app.route('/exercise/delete/<int:id>')
-def delete_exercise(id):
-    exercise = Exercise.query.get_or_404(id)
-    db.session.delete(exercise)
-    db.session.commit()
-    return redirect(url_for('index'))
+init_routes(app) # import routes
 
 # Create tables if necessary
 with app.app_context():
