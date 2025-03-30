5f6fdd2af750445a82abfc3077b74ac7991099cb
elianpullen <elianpullen@protonmail.com>
2025-03-30 12:58:37 +0200

    wip: user model

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
-    is_admin = db.Column(db.Boolean, nullable=False, default=False)
+    is_admin = db.Column(db.Integer, nullable=False, default=0)
     bodyweight = db.Column(db.Float, nullable=True) 
     bodyfat = db.Column(db.Float, nullable=True)
 
