cc07f2d5c538e1547cbd9cd8bf34d20e364dcccf
elianpullen <elianpullen@protonmail.com>
2025-03-31 21:17:09 +0200

    bug: fix exercise index

--- routes.py
+++ routes.py
@@ -8,12 +8,12 @@ def routes(app):
 
     @app.route('/admin/')
     def admin():
-        exercises = Exercise.query.all()
-        return render_template('admin/index.html', exercises=exercises)
+        return render_template('admin/index.html')
 
     @app.route('/admin/exercise/')
     def exercise():
-        return render_template('admin/exercise/index.html')
+        exercises = Exercise.query.all()
+        return render_template('admin/exercise/index.html', exercises=exercises)
 
     @app.route('/admin/exercise/edit/<int:id>/', methods=['GET', 'POST'])
     def edit_exercise(id):
