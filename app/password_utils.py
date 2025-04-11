22b35ff95b2c3ea1cd1882a0b5d9a04bd4e0dffc
elianpullen <elianpullen@protonmail.com>
2025-04-11 13:34:13 +0200

    doc: add password_utils comments

--- app/password_utils.py
+++ app/password_utils.py
@@ -2,12 +2,12 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 class PasswordMixin:
     @property
-    def password(self):
+    def password(self): # make user.password non-readable
         raise AttributeError('password is not readable')
 
     @password.setter
-    def password(self, password):
+    def password(self, password): # set password via user.password with hash
         self.password_hash = generate_password_hash(password)
 
-    def verify_password(self, password):
+    def verify_password(self, password): # check if givven password matches stored hash
         return check_password_hash(self.password_hash, password)
