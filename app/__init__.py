6197adbc60f0232134a3af41c4e640dc3564721a
elianpullen <elianpullen@protonmail.com>
2025-04-14 17:04:24 +0200

    refctor: move config to init.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,23 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from .config import Config
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = 'supersecretkey'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.sqlite')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
-    app.config.from_object(config_class)
+    app.config.from_object(config_class) # Get config from config.py
 
+    # Login manager
     db.init_app(app)
     login_manager.init_app(app)
     login_manager.login_view = "main.login"
