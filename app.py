a3f5af40a75de9eba807d5d99d36dc0b448855f4
elianpullen <elianpullen@protonmail.com>
2025-04-16 23:20:13 +0200

    feat: create default cardio category

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import getpass
 from app import db, create_app
-from app.models import User
+from app.models import User, Category
 from werkzeug.security import generate_password_hash
 
 class DatabaseInitializer:
@@ -35,11 +35,25 @@ class DatabaseInitializer:
                 print("Standaard admin-account aangemaakt!")
             else:
                 print("Admin-account bestaat al! Geen wijzigingen aangebracht.")
+    
+    def create_default_category(self):
+        """Create a default category if one doesn't exist."""
+        with self.app.app_context():
+            default_category = Category.query.filter_by(name='Cardio').first()
+
+            if not default_category:
+                default_category = Category(name='Cardio')
+                db.session.add(default_category)
+                db.session.commit()
+                print("Cardio categorie aangemaakt!")
+            else:
+                print("Standaard Cardio categorie bestaat al! Geen wijzigingen aangebracht.")
 
     def initialize_database(self):
         """Run all database initialization steps."""
         self.create_tables()
         self.create_admin()
+        self.create_default_category()
 
 if __name__ == "__main__": # Only run this if this file is being run directly
     initializer = DatabaseInitializer()# Initialize database first
